7e289654b48d233fd417bbc035198f657a11f9d7
Denis Greckij <denisgreckij38@gmail.com>
2022-05-21 14:35:15 +0300

    feat: solve task 4

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,23 @@
+const fs = require("fs");
+const path = require("path");
+const pathToFolder = path.join(__dirname, "files");
+const pathToFolderCopy = path.join(__dirname, "files-copy");
 
+fs.mkdir(pathToFolderCopy, {recursive: true} , (err) => {
+  if (err) console.log(err);
+  else {
+    fs.readdir(pathToFolder, (err, files) => {
+      if (err) console.log(err);
+      else {
+        files.forEach(file => {
+          fs.copyFile(path.join(pathToFolder, file), path.join(pathToFolderCopy, file),
+            (err) => {
+              if (err) console.log(err);
+              else console.log(`File ${file} copied sucessfully`);
+            } 
+          );
+        });
+      }
+    });
+  }
+});
