eb02bbf803387cd67038a357571f77d894ff274f
Denis Greckij <denisgreckij38@gmail.com>
2022-05-22 13:31:12 +0300

    feat: recursive copy

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,23 +1,41 @@
 const fs = require("fs");
+const promise = require("fs/promises");
 const path = require("path");
 const pathToFolder = path.join(__dirname, "files");
 const pathToFolderCopy = path.join(__dirname, "files-copy");
 
-fs.mkdir(pathToFolderCopy, {recursive: true} , (err) => {
-  if (err) console.log(err);
-  else {
-    fs.readdir(pathToFolder, (err, files) => {
-      if (err) console.log(err);
-      else {
-        files.forEach(file => {
-          fs.copyFile(path.join(pathToFolder, file), path.join(pathToFolderCopy, file),
+const copyDir = async (pathToFolder, pathToFolderCopy) => {
+  await promise.mkdir(pathToFolderCopy, {recursive: true});
+
+  fs.readdir(pathToFolderCopy, {withFileTypes: true}, (e, data) => {
+    if (e) console.log(e);
+    for (let el of data){
+      fs.access(path.join(pathToFolder, el.name), (err) => {
+        if (err){
+          fs.rm(path.join(pathToFolderCopy, el.name), {recursive: true}, (err) => {
+            if (err) console.log(err);
+          });
+        }
+      });
+    }
+  });
+
+  fs.readdir(pathToFolder, {withFileTypes: true}, (e, data) => {
+    if (e) console.log(e);
+    for (let el of data){
+      if (el.isFile()){
+        fs.copyFile(path.join(pathToFolder, el.name), path.join(pathToFolderCopy, el.name),
             (err) => {
               if (err) console.log(err);
-              else console.log(`File ${file} copied sucessfully`);
+              else console.log(`File ${el.name} copied sucessfully`);
             } 
-          );
-        });
+        );
       }
-    });
-  }
-});
+      if (el.isDirectory()){
+        copyDir(path.join(pathToFolder, el.name), path.join(pathToFolderCopy, el.name));
+      }
+    }
+  });
+}
+
+copyDir(pathToFolder, pathToFolderCopy);
